a03024a3b7a476cfebd03c55330e0e35022ec4ed
phb2000 <p.becker2000@hotmail.com>
2020-06-03 17:33:33 -0300

    formulario finalizado, pagina search

--- Ecoleta/scripts/create-point.js
+++ Ecoleta/scripts/create-point.js
@@ -15,6 +15,8 @@ populateUfs()
 
 function getCities(event) {
     const citySelect = document.querySelector("select[name=city")
+    citySelect.innerHTML = "<option value=''>Selecione uma cidade</option>"; // zera a lista de cidades
+    citySelect.disabled = true;
     const stateInput = document.querySelector("input[name=state]")
     const id = event.target.value
     let indexOfSelectedState = event.target.selectedIndex;
@@ -24,7 +26,7 @@ function getCities(event) {
 
     fetch(url).then((res) => { return res.json() }).then((cities) => {
         for (let city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false;
@@ -33,3 +35,48 @@ function getCities(event) {
 
 
 document.querySelector("select[name=uf]").addEventListener("change", getCities)
+
+//itens de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li") //armazena todos os items li em um array
+
+for (const item of itemsToCollect) { //percorre o array.
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    //add or remove a class with js
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id;
+    //console.log(itemId);
+
+    //verificar se existem itens selecionados e pega-los
+
+    const alreadySelected = selectedItems.findIndex((item) => {
+        return item == itemId;
+    })
+
+    //se já estiver selecionado, 
+    if (alreadySelected >= 0) {
+        //tirar da selecao
+        const filteredItems = selectedItems.filter(item => {
+            const filter = item != itemId
+            return filter;
+        })
+
+        selectedItems = filteredItems
+    } else {
+        //se nao estiver selecionado, remover da selecao
+        selectedItems.push(itemId)
+    }
+
+    //console.log(selectedItems);
+
+    //atualizar o campo escondido
+    collectedItems.value = selectedItems
+
+}
